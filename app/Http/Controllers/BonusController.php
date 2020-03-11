HDInnovations/UNIT3D-Community-Edition

commit 57212ca2631307c15ad6b9327129b82df6a25e84

tests: Add BonusController tests

app/Http/Controllers/BonusController.php
@@ -227,14 +227,14 @@ class BonusController extends Controller
      * @param \Illuminate\Http\Request $request
      * @param                          $id
      *
-     * @return Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function exchange(Request $request, $id)
     {
         $user = $request->user();
         $userbon = $user->seedbonus;
 
-        $BonExchange = new BonExchange();
+        $BonExchange = resolve(BonExchange::class);
         $itemCost = $BonExchange->getItemCost($id);
 
         if ($userbon >= $itemCost) {
@@ -271,7 +271,7 @@ class BonusController extends Controller
 
         $user_acc = User::findOrFail($userID);
         $activefl = PersonalFreeleech::where('user_id', '=', $user_acc->id)->first();
-        $bon_transactions = new BonTransactions();
+        $bon_transactions = resolve(BonTransactions::class);
 
         if ($item['upload'] == true) {
             $user_acc->uploaded += $item['value'];
@@ -324,7 +324,7 @@ class BonusController extends Controller
      *
      * @param \Illuminate\Http\Request $request
      *
-     * @return Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function sendGift(Request $request)
     {
@@ -415,7 +415,7 @@ class BonusController extends Controller
      * @param \Illuminate\Http\Request $request
      * @param                          $id
      *
-     * @return Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function tipUploader(Request $request, $id)
     {
@@ -465,15 +465,14 @@ class BonusController extends Controller
      *
      * @param \Illuminate\Http\Request $request
      *
-     * @return Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function tipPoster(Request $request)
+    public function tipPoster(Request $request, $id)
     {
         $user = $request->user();
 
         if ($request->has('post') && $request->input('post') > 0) {
-            $p = (int) $request->input('post');
-            $post = Post::with('topic')->findOrFail($p);
+            $post = Post::with('topic')->findOrFail($id);
             $poster = User::where('id', '=', $post->user_id)->firstOrFail();
         } else {
             abort(404);

tests/Feature/Http/Controllers/BonusControllerTest.php
@@ -1,0 +1,259 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\BonExchange;
+use App\Models\Post;
+use App\Models\Torrent;
+use App\Models\User;
+use BotsTableSeeder;
+use ChatroomTableSeeder;
+use GroupsTableSeeder;
+use Tests\TestCase;
+use UsersTableSeeder;
+
+/**
+ * @see \App\Http\Controllers\BonusController
+ */
+class BonusControllerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(GroupsTableSeeder::class);
+    }
+
+    /** @test */
+    public function bonus_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('bonus'));
+
+        $response->assertOk()
+            ->assertViewIs('bonus.index')
+            ->assertViewHas('userbon')
+            ->assertViewHas('dying')
+            ->assertViewHas('legendary')
+            ->assertViewHas('old')
+            ->assertViewHas('huge')
+            ->assertViewHas('large')
+            ->assertViewHas('regular')
+            ->assertViewHas('participant')
+            ->assertViewHas('teamplayer')
+            ->assertViewHas('committed')
+            ->assertViewHas('mvp')
+            ->assertViewHas('legend')
+            ->assertViewHas('total')
+            ->assertViewHas('daily')
+            ->assertViewHas('weekly')
+            ->assertViewHas('monthly')
+            ->assertViewHas('yearly')
+            ->assertViewHas('username')
+            ->assertViewHas('minute')
+            ->assertViewHas('second');
+    }
+
+    /** @test */
+    public function upload_exchange_returns_an_ok_response()
+    {
+        // User's seed bonus must be >= cost for exchange to succeed.
+
+        $user = factory(User::class)->create([
+            'seedbonus' => 2,
+        ]);
+
+        $bon = factory(BonExchange::class)->create([
+            'cost'               => 1,
+            'upload'             => true,
+            'download'           => false,
+            'personal_freeleech' => false,
+            'invite'             => false,
+        ]);
+
+        $response = $this->actingAs($user)->post(route('bonus_exchange', ['id' => $bon->id]));
+
+        $response->assertRedirect(route('bonus_store'))
+            ->assertSessionHas('success', 'Bonus Exchange Successful');
+    }
+
+    /** @test */
+    public function download_exchange_returns_an_ok_response()
+    {
+        // User's seed bonus must be >= cost for exchange to succeed.
+
+        // Likewise, User's downloaded value must be >= Bon value.
+
+        $user = factory(User::class)->create([
+            'seedbonus'  => 2,
+            'downloaded' => 2,
+        ]);
+
+        $bon = factory(BonExchange::class)->create([
+            'cost'               => 1,
+            'value'              => 1,
+            'upload'             => false,
+            'download'           => true,
+            'personal_freeleech' => false,
+            'invite'             => false,
+        ]);
+
+        $response = $this->actingAs($user)->post(route('bonus_exchange', ['id' => $bon->id]));
+
+        $response->assertRedirect(route('bonus_store'))
+            ->assertSessionHas('success', 'Bonus Exchange Successful');
+    }
+
+    /** @test */
+    public function personal_freeleech_exchange_returns_an_ok_response()
+    {
+        // User's seed bonus must be >= cost for exchange to succeed.
+
+        // Likewise, User's downloaded value must be >= Bon value.
+
+        $user = factory(User::class)->create([
+            'seedbonus'  => 2,
+            'downloaded' => 2,
+        ]);
+
+        $bon = factory(BonExchange::class)->create([
+            'cost'               => 1,
+            'value'              => 1,
+            'upload'             => false,
+            'download'           => false,
+            'personal_freeleech' => true,
+            'invite'             => false,
+        ]);
+
+        $response = $this->actingAs($user)->post(route('bonus_exchange', ['id' => $bon->id]));
+
+        $response->assertRedirect(route('bonus_store'))
+            ->assertSessionHas('success', 'Bonus Exchange Successful');
+    }
+
+    /** @test */
+    public function gift_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('bonus_gift'));
+
+        $response->assertOk()
+            ->assertViewIs('bonus.gift')
+            ->assertViewHas('userbon');
+    }
+
+    /** @test */
+    public function gifts_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('bonus_gifts'));
+
+        $response->assertOk()
+            ->assertViewIs('bonus.gifts')
+            ->assertViewHas('user')
+            ->assertViewHas('gifttransactions')
+            ->assertViewHas('userbon')
+            ->assertViewHas('gifts_sent')
+            ->assertViewHas('gifts_received');
+    }
+
+    /** @test */
+    public function send_gift_returns_an_ok_response()
+    {
+        $this->seed(UsersTableSeeder::class);
+        $this->seed(BotsTableSeeder::class);
+        $this->seed(ChatroomTableSeeder::class);
+
+        $senderUser = factory(User::class)->create([
+            'seedbonus' => 2,
+        ]);
+
+        $recipientUser = factory(User::class)->create();
+
+        $response = $this->actingAs($senderUser)->post(route('bonus_send_gift'), [
+            'to_username'   => $recipientUser->username,
+            'bonus_message' => 'foo',
+            'bonus_points'  => 1,
+        ]);
+
+        $response->assertRedirect(route('bonus_gift'))
+            ->assertSessionHas('success', 'Gift Sent');
+    }
+
+    /** @test */
+    public function store_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('bonus_store'));
+
+        $response->assertOk()
+            ->assertViewIs('bonus.store')
+            ->assertViewHas('users')
+            ->assertViewHas('userbon')
+            ->assertViewHas('activefl')
+            ->assertViewHas('bontransactions')
+            ->assertViewHas('uploadOptions')
+            ->assertViewHas('downloadOptions')
+            ->assertViewHas('personalFreeleech')
+            ->assertViewHas('invite');
+    }
+
+    /** @test */
+    public function tip_poster_returns_an_ok_response()
+    {
+        // User's seed bonus must be >= tip amount for exchange to succeed.
+
+        $user = factory(User::class)->create([
+            'seedbonus' => 2,
+        ]);
+
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($user)->post(route('tip_poster', ['id' => $post->id]), [
+            'post' => $post->id,
+            'tip'  => '1',
+        ]);
+
+        $response->assertRedirect(route('forum_topic', ['id' => $post->topic->id]))
+            ->assertSessionHas('success', 'Your Tip Was Successfully Applied!');
+    }
+
+    /** @test */
+    public function tip_uploader_returns_an_ok_response()
+    {
+        // User's seed bonus must be >= tip amount for exchange to succeed.
+
+        $user = factory(User::class)->create([
+            'seedbonus' => 2,
+        ]);
+
+        $torrent = factory(Torrent::class)->create();
+
+        $response = $this->actingAs($user)->post(route('tip_uploader', ['id' => $torrent->id]), [
+            'tip' => 1,
+        ]);
+
+        $response->assertRedirect(route('torrent', ['id' => $torrent->id]))
+            ->assertSessionHas('success', 'Your Tip Was Successfully Applied!');
+    }
+
+    /** @test */
+    public function tips_returns_an_ok_response()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('bonus_tips'));
+
+        $response->assertOk()
+            ->assertViewIs('bonus.tips')
+            ->assertViewHas('user')
+            ->assertViewHas('bontransactions')
+            ->assertViewHas('userbon')
+            ->assertViewHas('tips_sent')
+            ->assertViewHas('tips_received');
+    }
+}

tests/Todo/Feature/Http/Controllers/BonusControllerTest.php
@@ -1,209 +1,0 @@
-<?php
-
-namespace Tests\Feature\Http\Controllers;
-
-use Tests\TestCase;
-
-/**
- * @see \App\Http\Controllers\BonusController
- */
-class BonusControllerTest extends TestCase
-{
-    /**
-     * @test
-     */
-    public function bonus_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->get(route('bonus'));
-
-        $response->assertOk();
-        $response->assertViewIs('bonus.index');
-        $response->assertViewHas('userbon');
-        $response->assertViewHas('dying');
-        $response->assertViewHas('legendary');
-        $response->assertViewHas('old');
-        $response->assertViewHas('huge');
-        $response->assertViewHas('large');
-        $response->assertViewHas('regular');
-        $response->assertViewHas('participant');
-        $response->assertViewHas('teamplayer');
-        $response->assertViewHas('committed');
-        $response->assertViewHas('mvp');
-        $response->assertViewHas('legend');
-        $response->assertViewHas('total');
-        $response->assertViewHas('daily');
-        $response->assertViewHas('weekly');
-        $response->assertViewHas('monthly');
-        $response->assertViewHas('yearly');
-        $response->assertViewHas('username');
-        $response->assertViewHas('minute');
-        $response->assertViewHas('second');
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function exchange_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->post(route('bonus_exchange', ['id' => $id]), [
-            // TODO: send request data
-        ]);
-
-        $response->assertRedirect(withErrors('Bonus Exchange Failed!'));
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function gift_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->get(route('bonus_gift'));
-
-        $response->assertOk();
-        $response->assertViewIs('bonus.gift');
-        $response->assertViewHas('userbon');
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function gifts_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->get(route('bonus_gifts'));
-
-        $response->assertOk();
-        $response->assertViewIs('bonus.gifts');
-        $response->assertViewHas('user');
-        $response->assertViewHas('gifttransactions');
-        $response->assertViewHas('userbon');
-        $response->assertViewHas('gifts_sent');
-        $response->assertViewHas('gifts_received');
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function send_gift_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->post(route('bonus_send_gift'), [
-            // TODO: send request data
-        ]);
-
-        $response->assertRedirect(withErrors('Unable to find specified user'));
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function store_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->get(route('bonus_store'));
-
-        $response->assertOk();
-        $response->assertViewIs('bonus.store');
-        $response->assertViewHas('users');
-        $response->assertViewHas('userbon');
-        $response->assertViewHas('activefl');
-        $response->assertViewHas('bontransactions');
-        $response->assertViewHas('uploadOptions');
-        $response->assertViewHas('downloadOptions');
-        $response->assertViewHas('personalFreeleech');
-        $response->assertViewHas('invite');
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function tip_poster_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->post(route('tip_poster', ['id' => $id]), [
-            // TODO: send request data
-        ]);
-
-        $response->assertRedirect(withErrors('You Are To Broke To Tip The Poster!'));
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function tip_uploader_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->post(route('tip_uploader', ['id' => $id]), [
-            // TODO: send request data
-        ]);
-
-        $response->assertRedirect(withErrors('Your To Broke To Tip The Uploader!'));
-
-        // TODO: perform additional assertions
-    }
-
-    /**
-     * @test
-     */
-    public function tips_returns_an_ok_response()
-    {
-        $this->markTestIncomplete('This test case was generated by Shift. When you are ready, remove this line and complete this test case.');
-
-        $user = factory(\App\Models\User::class)->create();
-
-        $response = $this->actingAs($user)->get(route('bonus_tips'));
-
-        $response->assertOk();
-        $response->assertViewIs('bonus.tips');
-        $response->assertViewHas('user');
-        $response->assertViewHas('bontransactions');
-        $response->assertViewHas('userbon');
-        $response->assertViewHas('tips_sent');
-        $response->assertViewHas('tips_received');
-
-        // TODO: perform additional assertions
-    }
-
-    // test cases...
-}

bluesky66-dev/Aduro

commit 1f1167c87c2b705c15ca8a792f1418ee52bcab1f

fix: Fix regression
57212ca2631307c15ad6b9327129b82df6a25e84 introduced the following change:

https://github.com/HDInnovations/UNIT3D-Community-Edition/blob/52c3d557944f2cba286573df3fa42f617918ce4e/app/Http/Controllers/BonusController.php#L489

This broke Tipping as described in:

https://github.com/HDInnovations/UNIT3D-Community-Edition/issues/1193

app/Http/Controllers/BonusController.php
@@ -477,16 +477,15 @@ class BonusController extends Controller
      * Tip Points To A Poster.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Post         $id
      *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function tipPoster(Request $request, $id)
+    public function tipPoster(Request $request)
     {
         $user = $request->user();
 
         if ($request->has('post') && $request->input('post') > 0) {
-            $post = Post::with('topic')->findOrFail($id);
+            $post = Post::with('topic')->findOrFail($request->input('post'));
             $poster = User::where('id', '=', $post->user_id)->firstOrFail();
         } else {
             abort(404);

grumphp.yml
@@ -24,7 +24,7 @@ parameters:
             - fix
             - wip
         - scopes: []
-      # max_body_width: 72
+      max_body_width: 0
       max_subject_width: 72
       case_insensitive: false
       multiline: true

resources/views/forum/topic.blade.php
@@ -136,7 +136,7 @@
                                         <div class="some-padding">@lang('forum.tip-post-total') {{ $p->tips->sum('cost') }}
                                             BON</div>
                                     @endauth
-                                    <div class="some-padding" id="forumTip" route="{{ route('tip_poster', ['id' => $p->id]) }}"
+                                    <div class="some-padding" id="forumTip" route="{{ route('tip_poster') }}"
                                             leaveTip="@lang('torrent.leave-tip')" quickTip="@lang('torrent.quick-tip')">
                                         <a class="forumTip" href="#/" post="{{ $p->id }}"
                                             user="{{ $p->user->id }}">@lang('forum.tip-this-post')</a></div>

routes/web.php
@@ -501,7 +501,7 @@ Route::group(['middleware' => 'language'], function () {
             // Open Topic
             Route::get('/{id}/open', 'TopicController@openTopic')->name('forum_open');
             //
-            Route::post('/posts/{id}/tip_poster', 'BonusController@tipPoster')->name('tip_poster');
+            Route::post('/posts/tip_poster', 'BonusController@tipPoster')->name('tip_poster');
 
             // Edit Topic
             Route::get('/{id}/edit', 'TopicController@editForm')->name('forum_edit_topic_form');