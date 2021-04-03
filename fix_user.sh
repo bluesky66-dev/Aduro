48c2192df5185c974d6fb70bbf8eaf440d532470
bluesky66-dev <alister11david@gmail.com>
2021-04-03 16:51:46 +0800

    updated .github

---
+++ fix_user.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Note: You should also update the [user] group in .git/config
+# Note: This does not play nicely with remotes. You will need to blow them away and start over :(
+# ref: https://stackoverflow.com/questions/750172/change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-git
+
+git filter-branch --env-filter '
+OLD_EMAIL="iam@mdeangelo272.me"
+CORRECT_NAME="Michael DeAngelo"
+CORRECT_EMAIL="mdeangelo@trace3.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
